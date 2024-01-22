09c9ebf8f8c9ece97a0750bc43f292338155f80f
Talha Ahmed <talhaahmed@untoldstudios.tv>
2024-01-22 11:28:19 +0500

    Change jq command to filter out erroring jsons

--- _utm_json.sh
+++ _utm_json.sh
@@ -27,26 +27,44 @@ _utm_json_initialize() {
 _utm_json_add_repos () {
   local task="$1"
   local repos=("$@")
-
 }
 
 _utm_json_task_by_status () {
   local status=$1
 
-  local cmd="jq -r '"
-  if [ -n "$status" ]; then
-    cmd="$cmd select(.status==\$status) |"
-  fi
-  cmd="$cmd input_filename | split(\"/\")[-2] '"
+  local cmd="jq -n -r "
 
+  # Adding json files and data as arguments
+  local json_file
+  for json_file in "$UTM_TASKDIR"/*/"$_UTM_JSON_FILENAME"; do
+    data="$(<"$json_file")"
+    cmd="$cmd --arg \"$json_file\" \"\$(<$json_file)\""
+  done
+
+  # Adding status argument
   if [ -n "$status" ]; then
     cmd="$cmd --arg status $status"
   fi
 
-  local json_file
-  for json_file in "$UTM_TASKDIR"/*/"$_UTM_JSON_FILENAME"; do
-    cmd="$cmd \"$json_file\""
-  done
+  cmd="$cmd '"
+
+  # getting each task_file and task_name
+  cmd="$cmd \$ARGS.named"
+  cmd="$cmd | keys | .[] | select(.!=\"status\") | . as \$task_file"
+  cmd="$cmd | split(\"/\")[-2] as \$task_name"
+
+  # filtering out all invalid jsons
+  cmd="$cmd | \$ARGS.named[\$task_file] | try fromjson catch empty"
+  cmd="$cmd | select(.name==\$task_name)"
+
+  # add status filter if required
+  if [ -n "$status" ]; then
+    cmd="$cmd | select(.status==\$status)"
+  fi
+
+  # getting task name from filename
+  cmd="$cmd | \$task_file | split(\"/\")[-2]"
+  cmd="$cmd'"
 
   _utm_log_debug "$cmd"
   eval "$cmd"
