a3ba90e8c3c36272f53e85f702eef174940bb72a
Talha Ahmed <talhaahmed@untoldstudios.tv>
2024-02-23 11:39:48 +0500

    utm list completions to complete flags always

--- _utm_list.sh
+++ _utm_list.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+
 _UTM_LIST_FLAGS=(
   "--retired" "-r"
   "--all" "-a"
 )
+
 
 _utm_list_completions() {
   local words=("$@")  
   local num_words=${#words[@]}
 
   if [ "$num_words" -eq 1 ]; then
-    _utm_suggest "${words[0]}" "" "${_UTM_LIST_FLAGS[*]}"
+    _utm_suggest "${words[0]}" "${_UTM_LIST_FLAGS[*]}" "${_UTM_LIST_FLAGS[*]}"
   fi
 }
+
 
 _utm_list() {
   local flag=$1
